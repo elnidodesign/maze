7098940a8ac84432517b14402915402ac0b4127d
Oscar Diaz <oscar@designdiaz.com>
2013-10-23 00:57:59 -0400

    initial project version

---
+++ js/slider/functions.js
@@ -0,0 +1,27 @@
+// =============== Flex slider functions =============== //
+
+	function flexSliderCaption(){
+		$('.flexslider .slides li')
+			.mouseenter(function(){
+				$(this).find('.flex-caption').stop().animate({
+						opacity : 1
+					} , 500);
+			})
+			.mouseleave(function(){
+				$(this).find('.flex-caption').stop().animate({
+						opacity : 0
+					} , 500);
+			});	
+	}
+	
+	function setupFlexSlider(){
+		 $('.flexslider').flexslider({
+			animation: "slide",
+			/*direction: "vertical",    */            
+			slideshow: false, 
+			startAt: 1
+		  });
+	}
+	
+// =============== Flex slider functions END =============== //
+
